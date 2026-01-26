95d69eba0a85a6454cdb2f324e88e79a543bfa6f
Mariea03 <110367002+Mariea03@users.noreply.github.com>
2026-01-25 21:46:31 -0500

    Initial commit: apre-server with Sales by Salesperson API

---
+++ src/routes/reports/sales/index.js
@@ -0,0 +1,135 @@
+  /**
+ * Author: Professor Krasso
+ * Date: 8/14/24
+ * File: index.js
+ * Description: Apre sales report API for the sales reports
+ */
+
+'use strict';
+
+const express = require('express');
+const { mongo } = require('../../../utils/mongo');
+
+const router = express.Router();
+
+/**
+ * @description
+ *
+ * GET /regions
+ *
+ * Fetches a list of distinct sales regions.
+ *
+ * Example:
+ * fetch('/regions')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+router.get('/regions', (req, res, next) => {
+  try {
+    mongo (async db => {
+      const regions = await db.collection('sales').distinct('region');
+      res.send(regions);
+    }, next);
+  } catch (err) {
+    console.error('Error getting regions: ', err);
+    next(err);
+  }
+});
+
+/**
+ * @description
+ *
+ * GET /regions/:region
+ *
+ * Fetches sales data for a specific region, grouped by salesperson.
+ *
+ * Example:
+ * fetch('/regions/north')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+router.get('/regions/:region', (req, res, next) => {
+  try {
+    mongo (async db => {
+      const salesReportByRegion = await db.collection('sales').aggregate([
+        { $match: { region: req.params.region } },
+        {
+          $group: {
+            _id: '$salesperson',
+            totalSales: { $sum: '$amount'}
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            salesperson: '$_id',
+            totalSales: 1
+          }
+        },
+        {
+          $sort: { salesperson: 1 }
+        }
+      ]).toArray();
+      res.send(salesReportByRegion);
+    }, next);
+  } catch (err) {
+    console.error('Error getting sales data for region: ', err);
+    next(err);
+  }
+});
+
+/**
+ * Mariea Nies Added -
+ * GET / salesperson
+ *
+ * Fetches total sales grouped by salesperson.
+ *
+ * Example:
+ * fetch('/salesperson')
+ *  .then(response => response.json())
+ *  .then(data => console.log(data));
+ */
+
+router.get('/salesperson', (req, res, next) => {
+
+    try{
+      const { salesperson } = req.params;
+
+      // Date 6 months ago
+      const sixMonthsAgo = new Date();
+      sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
+
+    mongo(async db => {
+      const salesBySalesperson = await db.collection('sales').aggregate([
+        {
+          $match:{
+            salesperson: salesperson,
+            saleDate: { $gte: sixMonthsAgo}
+          }
+        },
+        {
+          $group: {
+            _id: '$salesperson',
+            totalSales: { $sum: '$amount'}
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            salesperson: '$_id',
+            totalSales: 1
+          }
+        },
+        {
+          $sort: { salesperson: 1 }
+        }
+      ]).toArray();
+
+      res.send(sales);
+    }, next);
+  } catch (err) {
+    console.error('Error fetching sales by salesperson:', err);
+  }
+});
+
+module.exports = router;
